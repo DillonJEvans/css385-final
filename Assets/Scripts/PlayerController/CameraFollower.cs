c599dcfc7f873f1f847003ec0fa10fd046d49bd8
JJ H <73252837+jameshartog@users.noreply.github.com>
2024-05-12 01:30:56 -0700

    Script updates

---
+++ Assets/Scripts/PlayerController/CameraFollower.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+
+public class CameraFollower : MonoBehaviour
+{
+    public PlayerController player;
+    [Range(-0.5f, 0.5f)]
+    public float lookDirectionWeight;
+    [Min(0f)]
+    public float floatiness;
+    public bool stop;
+
+
+    private Vector3 velocity = Vector3.zero;
+
+
+    private void FixedUpdate()
+    {
+        if (!stop)
+        {
+            Vector3 target = TargetPosition();
+            transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, floatiness);
+        }
+    }
+
+
+    private Vector3 TargetPosition()
+    {
+        return new(
+            player.transform.position.x + lookDirectionWeight * player.lookDirection.x,
+            player.transform.position.y + lookDirectionWeight * player.lookDirection.y,
+            transform.position.z
+        );
+    }
+}
